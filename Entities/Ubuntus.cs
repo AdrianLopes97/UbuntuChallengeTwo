ec2632dc04fd18aa4f85334f0757b481d4b7a412
AdrianLopes97 <git.qodeless@outlook.com>
2021-07-04 21:22:34 -0300

    Resolução, enunciado está em JPEG na pasta file

---
+++ Entities/Ubuntus.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewChallengeTwo_CSharp.Entities
+{
+    public class Ubuntus
+    {
+
+        //Class properties, here!
+        #region classProperties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public decimal Height { get; set; }
+        public decimal Bmi { get; set; }
+        public bool IsDev { get; set; }
+        #endregion
+
+        //Constructor (go to Google)
+        #region Constructor
+        public Ubuntus()
+        {
+        }
+        public Ubuntus(string name, int age, double weight, decimal height, bool isDev)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+            Height = height;
+            IsDev = isDev;
+            Bmi = (decimal)weight / (height * height);
+        }
+        #endregion
+
+        //Class methods/functions
+
+        #region Methods/Functions
+        public static List<Ubuntus> CreateList()
+        {
+            //Object instance
+            var ubuntus = new List<Ubuntus>();
+
+            //Creating list
+            ubuntus.Add(new Ubuntus("Ubuntu silva", 33,88.50,1.65M,true));
+            ubuntus.Add(new Ubuntus("Ubuntu Santos", 26, 84.50, 2.10M, true));
+            ubuntus.Add(new Ubuntus("Ubuntu3 Silva", 85, 66.50, 1.40M, false));
+
+
+            return ubuntus;
+        }
+
+        public static string GroupNames()
+        {
+            string groupNames = "Os nome são:\n";
+            var ubuntus = CreateList();
+            foreach(var ubuntu in ubuntus)
+            {
+                groupNames += "\t*" + ubuntu.Name + "\n";
+            }
+            return groupNames;
+        }
+
+        public static string VerifyNames()
+        {
+            string groupNames = "Os Silvas: \n";
+            var ubuntus = CreateList();
+            foreach (var ubuntu in ubuntus)
+            {
+                //TOUPPER = avoid sensitive case
+                if (ubuntu.Name.ToUpper().Contains("siLvA".ToUpper()))
+                {
+                    groupNames += "\t*" + ubuntu.Name + "\n";
+                }
+                
+            }
+            return groupNames;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using NewChallengeTwo_CSharp.Entities;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace NewChallengeTwo_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start
+            Console.WriteLine("UBUNTU 002 - STARS...");
+
+            //Assigning List
+            var ubuntus = Ubuntus.CreateList();
+
+            //Sum ages from the list
+            int SumOfAge = ubuntus.Sum(_ua => _ua.Age);
+
+            //Print SUM of age ChallengeTwoTask001
+            Console.WriteLine("\nA soma das idades é " + SumOfAge + "\n");
+
+            //Print GROUP NAMES of age ChallengeTwoTask002
+            Console.WriteLine(Ubuntus.GroupNames());
+
+            //Print Average IBM ChallengeTwoTask003
+            //IMC = Weight / (height x height)
+            //Average = (IMC+IMC+IMC)/ubuntus.count            
+            Console.WriteLine("Média de IMC = " + (ubuntus.Sum(_ub => _ub.Bmi)/ubuntus.Count)
+                .ToString("F2", CultureInfo.InvariantCulture) + "Kg/m²\n");
+
+            //Print dev count ChallengeTwoTask004
+            Console.WriteLine("Possuí " + ubuntus.Where(_ud => _ud.IsDev).Count() + " devs no time!\n");
+
+            //Print name that contains silva ChallengeTwoTask005
+            Console.WriteLine(Ubuntus.VerifyNames());
+
+            Console.WriteLine("\nUBUNTU: THE  BETTER YOU ARE THE BETTER I AM!");
+            Console.WriteLine("\n\n\tVAMO DALE PARA NÂO TOMALE!\n\t\tVAMOS VIVER!\n\t\t\tNÃO APENAS SOBREVIVER!");
+
+
+        }
+    }
+}
